c16a3fd856f5d120db0ed6b3459e8427db12cb4d
Marshal1101 <adequateplayer9@gmail.com>
2021-12-22 19:59:46 +0900

    13까지

--- programmers/13_heap_03_dobblequeue.py.py
+++ programmers/13_heap_03_dobblequeue.py.py
@@ -16,16 +16,16 @@ def solution(operation):
     heap_cnt = 0
     for oper in operation :
         # 문자와 숫자 split
-        splited = oper.split(' ')
-        if splited[0] == "I" :
-            heappush(min_heap, int(splited[1]))
-            heappush(max_heap, -int(splited[1]))
+        order, num = oper.split(' ')
+        if order == "I" :
+            heappush(min_heap, int(num))
+            heappush(max_heap, -int(num))
             heap_cnt += 1
-        elif splited[0] == "D" :
+        elif order == "D" :
             if heap_cnt > 0 :
-                if splited[1] == "-1" :
+                if num == "-1" :
                     heappop(min_heap)
-                elif splited[1] == "1" :
+                elif num == "1" :
                     heappop(max_heap)
                 heap_cnt -= 1
                 # 힙 카운트 0이 되면 한 번은 pop이 되었던 숫자들만 남음
@@ -41,3 +41,35 @@ def solution(operation):
     return answer
 
 print(solution(operation))
+
+
+
+
+# 바이섹트 인소트
+
+import bisect
+
+def solution(operations):
+    num = []
+    for op in operations:
+        op = op.split()
+        
+        # Insert 명령
+        if op[0] == "I":
+            # 아무것도 없으면 그냥 넣고
+            if len(num) == 0: num.append(int(op[1]))
+            else: bisect.insort(num, int(op[1])) 
+        # Delete 명령
+        else:
+            if len(num) == 0: continue
+            # 최소값 제거
+            if op[1] == "-1":
+                num.pop(0)
+            # 최대값 제거
+            else:
+                num.pop()
+    # 결과 반환
+    if len(num) == 0: 
+        return [0,0]
+    else: 
+        return [num[-1], num[0]]
