a2091f53c7e1636c06a0a65da43d4309a44cd22b
Marshal1101 <adequateplayer9@gmail.com>
2022-07-18 20:17:59 +0900

    CLASS_5++ solving-9

--- baekjoon/CLASS_5++/018-2473-세용액.py
+++ baekjoon/CLASS_5++/018-2473-세용액.py
@@ -1,5 +1,4 @@
 import sys
-from bisect import bisect_left
 
 
 def main() :
@@ -8,36 +7,33 @@ def main() :
     N = int(input())
     arr = list(map(int, input().split()))
     arr.sort()
-    lp = 0
-    rp = N - 1
-    num1 = 0
-    num2 = 0
-    num3 = 0
-    min_gap = sys.maxsize
-    while lp + 1 < rp :
-        lr = arr[lp] + arr[rp]
-        p = bisect_left(arr[lp+1:rp], -lr)
-        ep = rp - lp
-        if 0 < p < ep - 1 :
-            temp1 = lr + arr[(mp := lp + p)]
-            temp2 = lr + arr[(mp := lp+1 + p)]
-            s = temp1 if abs(temp1) < abs(temp2) else temp2 
-        else :
-            s = lr + arr[(mp := lp + 1)]
 
-        if abs(s) < min_gap :
-            min_gap = s
-            num1 = arr[lp]
-            num2 = arr[rp]
-            num3 = arr[mp]
-        if s > 0 :
-            rp -= 1
-        elif (s == 0) :
-            return (arr[lp], arr[mp], arr[rp])
-        else :
-            lp += 1
+    left = 0 ; right = 0 ; mid = 0
+    min_sum = sys.maxsize
+    flag = False
+    for i in range(N-2) :
+        if flag : break
+        fix = arr[i]
+        lp = i + 1
+        rp = N - 1
+        while lp < rp :    
+            cur_sum = fix + arr[lp] + arr[rp]
+        
+            if abs(cur_sum) < min_sum :
+                min_sum = abs(cur_sum)
+                left = fix
+                mid = arr[lp]
+                right = arr[rp]
 
-    return (num1, num3, num2)
+            if cur_sum < 0 :
+                lp += 1
+            elif cur_sum > 0 :
+                rp -= 1
+            else :
+                flag = True
+                break
+
+    return (left, mid, right)
 
 if __name__ == '__main__' :
     print(*main())
--- baekjoon/CLASS_5++/020-7579-앱.py
+++ baekjoon/CLASS_5++/020-7579-앱.py
@@ -7,6 +7,27 @@ def main() :
     m = list(map(int, input().split()))
     c = list(map(int, input().split()))
 
+    TC = sum(c)
+
+    dp = [[ 0 for _ in range(TC+1)] for _ in range(N)]
+    for i in range(N) :
+        mem, cost = m[i], c[i]
+        if cost == 0 : dp[i][0] = mem
+        for j in range(1, TC+1) :
+            a = dp[i-1][j]
+            b = dp[i-1][j-cost] + mem
+            if j >= cost :
+                dp[i][j] = b if b > a else a
+            else :
+                dp[i][j] = a
+
+    # for p in dp :
+    #     print(p)
+    for j in range(TC+1) :
+        for i in range(N) :
+            if dp[i][j] >= M :
+                print(j)
+                exit()
 
 
 if __name__ == '__main__' :
