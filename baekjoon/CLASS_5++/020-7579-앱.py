0234bb1ae8c8ee4a10d53ed2c673bfd15f64d029
Marshal1101 <adequateplayer9@gmail.com>
2022-07-19 11:01:09 +0900

    CLASS_5++ solving-9

--- baekjoon/CLASS_5++/020-7579-앱.py
+++ baekjoon/CLASS_5++/020-7579-앱.py
@@ -7,23 +7,23 @@ def main() :
     m = list(map(int, input().split()))
     c = list(map(int, input().split()))
 
-    TC = sum(c)
+    c_tot = sum(c)
 
-    dp = [[ 0 for _ in range(TC+1)] for _ in range(N)]
+    dp = [[ 0 for _ in range(c_tot+1)] for _ in range(N)]
     for i in range(N) :
         mem, cost = m[i], c[i]
         if cost == 0 : dp[i][0] = mem
-        for j in range(1, TC+1) :
+        for j in range(1, c_tot+1) :
             a = dp[i-1][j]
             b = dp[i-1][j-cost] + mem
             if j >= cost :
-                dp[i][j] = b if b > a else a
+                dp[i][j] = b if (b := dp[i-1][j-cost] + mem) > (a := dp[i-1][j]) else a
             else :
-                dp[i][j] = a
+                dp[i][j] = dp[i-1][j]
 
     # for p in dp :
     #     print(p)
-    for j in range(TC+1) :
+    for j in range(c_tot+1) :
         for i in range(N) :
             if dp[i][j] >= M :
                 print(j)
