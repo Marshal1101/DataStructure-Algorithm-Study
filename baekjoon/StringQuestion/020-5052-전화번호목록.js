759bca6182e980b1af950dcc061b3a0808729d83
Marshal1101 <adequateplayer9@gmail.com>
2022-11-15 00:29:55 +0900

    JS class, object

--- baekjoon/StringQuestion/020-5052-전화번호목록.js
+++ baekjoon/StringQuestion/020-5052-전화번호목록.js
@@ -1,35 +1,80 @@
+// 메모리초과
+
+
 const [T, ...arr] = require('fs').readFileSync('z.in').toString().trim().split('\n');
 
-const hasThisFromTheFirst = (src, search) => {
-    const leng = src.length < search.length ? src.length : search.length;
 
-    for (let i = 0; i < leng; i++) {
-        if (src[i] != search[i]) return false;
+/** key(Character): a character of the string, data(String): the string, children(Object): next characters */
+class Node {
+    constructor(key, data=null) {
+        this.key = key;
+        this.data = data;
+        this.children = {};
     }
-
-    return true;
 }
 
-let result = [];
-let s_idx = 0;
-let e_idx = 0;
-let N_idx = 0;
-for (let i = 0; i < T; i++) {
-    e_idx = N_idx + arr[N_idx];
-    s_idx = N_idx + 1
-    let flag = false
-    for (let j = s_idx; j <= e_idx; j++) {
-        if (flag) break;
-        for (let k = j; k > s_idx; k--) {
-            if (hasThisFromTheFirst(arr[j], arr[k])) {
-                result.push("NO");
-                flag = true;
-                break;
+/** Node Tree: insert(String): insert the string into tree, searchPrefix(String): check if the string is a prefix in the tree. */
+class Trie {
+    constructor(arr) {
+        // this.insert = this.insert.bind(this);
+        this.head = new Node(null);
+        if (arr) {
+            for (let i in arr) {
+                this.insert(arr[i])
             }
         }
     }
-    if (!flag) result.push("YES");
-    N_idx += e_idx + 1;
+
+    isEmpty(obj) {
+        if (Object.entries(obj).length == 0) return true;
+        else false;
+    }
+
+    insert(string) {
+        let currNode = this.head;
+        for (let i in string) {
+            if (!currNode.children.hasOwnProperty(string[i])) {
+                // console.log("currNode.children.hasOwnProperty=", string[i]);
+                currNode.children[string[i]] = new Node(string[i]);
+                // console.log("currNode.children:", currNode.children);
+            }
+            currNode = currNode.children[string[i]];
+        }
+        currNode.data = string;
+    }
+
+    searchPrefix(string) {
+        let currNode = this.head;
+        for (let i in string) {
+            currNode = currNode.children[string[i]];
+            // console.log("search_num:", string[i], "CUR_NODE", currNode)
+        }
+        if (!this.isEmpty(currNode.children)) return true;
+        else return false;
+    }
 }
 
-console.log(result.join('\n'));
+let result = "";
+let s_idx = 0;
+let e_idx = 0;
+for (let i = 0; i < T; i++) {
+    e_idx = s_idx + Number(arr[s_idx]);
+    // console.log(s_idx, e_idx, arr[s_idx])
+    const nums = arr.slice(s_idx+1, e_idx+1);
+    nums.sort();
+    const trie = new Trie(arr);
+    // console.log("nums:", nums)
+    let flag = false
+    for (let n in nums) {
+        // console.log("num:", nums[n])
+        // console.log(trie)
+        if (trie.searchPrefix(nums[n])) {
+            flag = true;
+            break;
+        }
+    }
+    if (!flag) result += "YES\n";
+    else result += "NO\n"
+    s_idx = e_idx + 1;
+}
+console.log(result);
