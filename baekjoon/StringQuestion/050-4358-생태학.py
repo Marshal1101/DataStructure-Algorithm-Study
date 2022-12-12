48a048ddc7334be94c46c269344e66df1cc0edbb
Marshal1101 <adequateplayer9@gmail.com>
2022-12-12 22:19:15 +0900

    arr[][]=list(dict.items())

--- baekjoon/StringQuestion/050-4358-생태학.py
+++ baekjoon/StringQuestion/050-4358-생태학.py
@@ -7,8 +7,8 @@ cnt = 0
 while (treeName := input().rstrip()) != "":
     dicTree[treeName] += 1
     cnt += 1
-arrTree = dicTree.items()
-sortedTree = list(arrTree)
+
+sortedTree = list(dicTree.items())
 sortedTree.sort()
 for key, value in sortedTree:
     print(f"{key} {(value*100/cnt):.4f}")
