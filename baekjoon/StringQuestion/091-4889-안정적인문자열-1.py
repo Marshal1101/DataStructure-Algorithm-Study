df67a0059a3b1d1a70f748b1086ceaea3fe68b2b
HJ PARK <adequateplayer@gmail.com>
2023-01-07 21:44:45 +0900

    stack {}

---
+++ baekjoon/StringQuestion/091-4889-안정적인문자열-1.py
@@ -0,0 +1,31 @@
+import sys
+
+
+input = sys.stdin.readline
+
+N = 0
+stack = []
+while (line := input().rstrip())[0] != "-":
+    N += 1
+    cnt = 0
+    for c in line:
+        if not stack:
+            if c == "}":
+                cnt += 1
+                stack.append("{")
+            else: stack.append(c)
+
+        else:
+            if c == "}":
+                if stack[-1] == "{":
+                    stack.pop()
+                else: stack.append(c)
+            else:
+                stack.append(c)
+
+    while stack and stack[-1] == "{":
+        stack.pop() 
+        cnt += 1
+        stack.pop()
+
+    print(f"{N}. {cnt}")
