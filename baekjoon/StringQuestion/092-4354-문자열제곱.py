5869957fc363fca9fb9f13105de1b19254b87939
HJ PARK <adequateplayer@gmail.com>
2023-01-08 00:35:50 +0900

    KMP prefix_func matching

---
+++ baekjoon/StringQuestion/092-4354-문자열제곱.py
@@ -0,0 +1,27 @@
+import sys
+
+
+def compute_prefix(target: str) -> list:
+    M = len(target)
+    pi = [0] * M
+
+    k = 0
+    for q in range(1, M):
+        while k > 0 and target[k] != target[q]:
+            k = pi[k-1]
+        if target[k] == target[q]:
+            k += 1
+        pi[q] = k
+
+    return pi
+
+
+input = sys.stdin.readline
+while (s := input().rstrip()) != ".":
+    s.find
+    pi = compute_prefix(s)
+    if pi[-1] == 0: print(1)
+    else:
+        div = len(s) - pi[-1]
+        if pi[-1] % div == 0: print(len(s)//div)
+        else: print(1)
